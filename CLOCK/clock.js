7066a1d241ebc3c63e551d07af9299f95172ca04
Roshni Awasthi <91076174+munna0811@users.noreply.github.com>
2023-09-04 21:59:16 +0530

    Update clock.js

--- CLOCK/clock.js
+++ CLOCK/clock.js
@@ -10,4 +10,4 @@ setInterval(() => {
   hour.style.transform = `rotate(${hrotation}deg)`;
   minute.style.transform = `rotate(${mrotation}deg)`;
   second.style.transform = `rotate(${srotation}deg)`;
-}, 800);
+}, 1200);
